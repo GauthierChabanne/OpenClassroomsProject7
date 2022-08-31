42f701aa5c1b7d1a108594b81187e9e83f61f379
Gauthier Chabanne <gauthierchabanne@hotmail.fr>
2022-08-31 16:44:58 +0200

    search.js created

---
+++ scripts/outils/sort.js
@@ -0,0 +1,4 @@
+//DOM elements
+const cardSection = document.querySelector(".recipes_cards");
+const allCards = document.querySelectorAll(".recipe_card");
+const searchbarInput = document.querySelector("#searchbar_input")
