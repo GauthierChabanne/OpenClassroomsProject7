c44f5e1284bad3feea91066784e6f1aceda556e5
Gauthier Chabanne <gauthierchabanne@hotmail.fr>
2022-10-20 18:06:55 +0200

    Ameliorations

--- scripts/outils/mainSearchSort.js
+++ scripts/outils/mainSearchSort.js
@@ -6,32 +6,27 @@ const searchbarInput = document.querySelector("#searchbar_input");
 
 //sort function
 function forSort(cards, filter) {
-  //Regarde si il y a bien plus de deux caractères dans l'input principal
   if (filter.length > 2) {
     for (let x = 0; x < cards.length; x++) {
       cards[x].style.display = "none"
     }
-    //Regarde chacune des cartes une par une
+
     for (let i = 0; i < cards.length; i++) {
-      //Recupere le titre de la carte
+
       const cardTitle = cards[i].querySelector(".recipe_card__main_infos__name");
       const cardTitleValue = cardTitle.innerText;
-
-      //Recupere les ingrédients de la carte
       const cardIngredients = cards[i].querySelectorAll(".recipe_card__sub_infos__ingredients__ingredient__name");
       const cardIngredientsArray = [];
-      //Regarde chacun des ingrédients pour les stocker dans un arrêt
+
       for (let z = 0; z < cardIngredients.length; z++) {
         cardIngredientsArray.push(cardIngredients[z].innerText);
       }
-      // Transforme l'arrêt des ingrédients en string
+
       const cardIngredientsList = cardIngredientsArray.join(",");
 
-      //Recupere la description de la carte
       const cardDescription = cards[i].querySelector(".recipe_card__sub_infos__description");
       const cardDescriptionValue = cardDescription.innerText;
 
-      // Mélange les 3 dans un seul string
       const allCardContent = cardTitleValue + cardIngredientsList + cardDescriptionValue;
 
       if (allCardContent.toLowerCase().indexOf(filter) > -1) {
